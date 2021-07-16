6dbd0bf69cb10a7e60cfcf505356160aaba5ab28
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-07-16 17:08:52 +0530

    Advanced Stencil

--- Workspace/04-advanced-web-components-stencil/stencil.config.ts
+++ Workspace/04-advanced-web-components-stencil/stencil.config.ts
@@ -1,21 +1,14 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'advanced-web-components',
-  outputTargets: [
+  namespace: 'mycomponent',
+  outputTargets:[
     {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-    },
-    {
-      type: 'dist-custom-elements-bundle',
-    },
-    {
-      type: 'docs-readme',
+      type: 'dist'
     },
     {
       type: 'www',
-      serviceWorker: null, // disable service workers
-    },
-  ],
+      serviceWorker: null
+    }
+  ]
 };
