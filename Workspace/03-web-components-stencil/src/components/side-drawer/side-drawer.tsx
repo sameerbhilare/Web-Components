b925046067798766eaa0ff7ded71d5e0b554dd44
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-07-16 15:28:43 +0530

    Using Props decorator

--- Workspace/03-web-components-stencil/src/components/side-drawer/side-drawer.tsx
+++ Workspace/03-web-components-stencil/src/components/side-drawer/side-drawer.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from "@stencil/core";
+import { Component, h, Prop } from "@stencil/core";
 
 @Component({
   tag: 'uc-side-drawer',
@@ -9,10 +9,26 @@ import { Component, h } from "@stencil/core";
 export class SideDrawer {
 
   /*
+    Prop decorator adds automatic watcher, you could say.
+    Stencil will now watch for attributes named 'title' on our component
+    and if we set or change such an attribute, then stencil will detect this change
+    and it will automatically rerun the render() method
+    and it will do so in a very efficient manner so that it does not re-render the entire DOM
+    that was generated based on that but only the parts of the DOM that changed.
+  */
+  @Prop() title: string;
+
+  /*
     This is a method stencil will execute for us to parse the DOM it should generate as part of this component.
     And we have tp return JSX code
   */
   render() {
-    return <aside><h1>The Side Drawer</h1></aside>;
+    //return <aside><h1>The Side Drawer</h1></aside>;
+    return <aside>
+      <header><h1>{this.title}</h1></header>
+      <main>
+        <slot></slot>
+      </main>
+    </aside>
   }
 }
