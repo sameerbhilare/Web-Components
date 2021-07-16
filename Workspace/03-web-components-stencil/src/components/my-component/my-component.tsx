c63deeffb5f55843f932b8837add6e31d694c2ba
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-07-16 14:11:30 +0530

    Added useful comments

--- Workspace/03-web-components-stencil/src/components/my-component/my-component.tsx
+++ Workspace/03-web-components-stencil/src/components/my-component/my-component.tsx
@@ -1,6 +1,10 @@
 import { Component, Prop, h } from '@stencil/core';
 import { format } from '../../utils/utils';
 
+/*
+  Stencil automatically extends HTML Element
+*/
+
 @Component({
   tag: 'my-component',
   styleUrl: 'my-component.css',
@@ -27,6 +31,13 @@ export class MyComponent {
   }
 
   render() {
+    /*
+      Stencil.js contains a tool that parses below code
+      and converts it to Javascript that will then write it into the real DOM.
+      It has a couple of convenience features included,
+      for example it has a couple of features that will automatically rerun this render function
+      when you change some input to the render functions.
+    */
     return <div>Hello, World! I'm {this.getText()}</div>;
   }
 }
